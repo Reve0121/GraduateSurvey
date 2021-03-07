399f1d52b322af193c4d804034a87efb9b9451ba
F <291530919@qq.com>
2021-03-07 20:43:07 +0800

    更改配置文件

--- public/config.js
+++ public/config.js
@@ -7,12 +7,12 @@ const config = {
     /**
      * 服务端开发环境
      */
-    db_url: "mongodb://47.98.42.238:27017/graduate_survey_dev",
+    db_url: "mongodb://admin:admin%40Dxxy2021..@47.98.42.238:27017/graduate_survey_dev?authSource=admin", 
 
     /**
      * 服务端生产环境
      */
-    // db_url: "mongodb://47.98.42.238:27017/graduate_survey"
+    // db_url: "mongodb://admin:admin%40Dxxy2021..@47.98.42.238:27017/graduate_survey?authSource=admin"
 };
 
 module.exports = config
